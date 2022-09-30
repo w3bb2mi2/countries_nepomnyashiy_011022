f45ffc03a5ec1f95b1325a74fd709ba659ca14ac
Eduard <w3bb2mi2@gmail.com>
2022-09-30 08:52:06 +0300

    first commit

---
+++ src/component/Container.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    max-width: 12400px;
+    margin: 0 auto;
+    padding: 0 2rem;
+`
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
+import styled from 'styled-components'
+
+const HederEl = styled.header``
+export const Header = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
